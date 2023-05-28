fa265566ef33f3fa7d64131b41da01151eb5d853
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-05-28 18:16:40 +0530

    fix: project structure

--- tests/test_decrypt.py
+++ tests/test_decrypt.py
@@ -1,5 +1,5 @@
 import pytest
-from src.phase import Phase
+from src.phase.phase import Phase
 
 APP_ID = "phApp:v1:e0e50cb9a1953c610126b4092093b1beca51d08d91fc3d9f8d90482a32853215"
 APP_SECRET = "pss:v1:d261abecb6708c18bebdb8b2748ee574e2b0bdeaf19b081a5f10006cc83d48d0:d146c8c6d326a7842ff9b2da0da455b3f7f568a70808e2eb0cfc5143d4fe170f:59e413612e06d75d251e3416361d0743345a9c9eda1cbcf2b1ef16e3077c011c"
@@ -18,7 +18,7 @@ def mock_fetch_app_key(appToken, wrapKey, appId, dataSize):
 def test_phase_decrypt_returns_correct_plaintext(phase_instance, monkeypatch):
     data = "Signal"
 
-    monkeypatch.setattr("src.phase.fetch_app_key", mock_fetch_app_key)
+    monkeypatch.setattr("src.phase.phase.fetch_app_key", mock_fetch_app_key)
 
     ciphertext = phase_instance.encrypt(data)
 
@@ -31,7 +31,7 @@ def test_phase_decrypt_returns_correct_plaintext(phase_instance, monkeypatch):
 def test_phase_decrypt_fails_with_incorrect_app_secret(monkeypatch):
     phase = Phase(APP_ID, APP_SECRET_INVALID)
 
-    monkeypatch.setattr("src.phase.fetch_app_key", mock_fetch_app_key)
+    monkeypatch.setattr("src.phase.phase.fetch_app_key", mock_fetch_app_key)
 
     data = "Signal"
     ciphertext = phase.encrypt(data)
